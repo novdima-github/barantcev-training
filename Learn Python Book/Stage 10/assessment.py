6123cb9def13bc0b31fa061c81730cfd9bbd7761
novdima2 <d.navadvorski@feefo.com>
2020-01-09 15:27:26 +0300

    Assessment try except

--- Learn Python Book/Stage 10/assessment.py
+++ Learn Python Book/Stage 10/assessment.py
@@ -1,17 +1,18 @@
-f_users = """d:\\tutorials\\python\\barantcev-training\\barantcev-training\\Learn Python Book\\users.txt"""
+f_users = 'users.txt' #"""d:\\tutorials\\python\\barantcev-training\\barantcev-training\\Learn Python Book\\users.txt"""
 
 print('Hi. What is you name?')
 user_name = input()
 print('Hi. What is you hobby?')
 user_hobby = input()
 
-with open(f_users) as f_obj:
-    lines = f_obj.readlines()
-    if len(lines) == 0:
-        with open(f_users, 'a') as f_obj:
-            f_obj.write(str(1) + ' Name: ' + user_name.title() + ' Hobby: ' + user_hobby.title() + '\n')
-    else:
-        last_string = lines[len(lines)-1]
+try:
+    with open(f_users) as f_obj:
+        lines = f_obj.readlines()
+        last_string = lines[len(lines) - 1]
         id = int(last_string.split()[0])
         with open(f_users, 'a') as f_obj:
-            f_obj.write(str(id+1) + ' Name: ' + user_name.title() + ' Hobby: ' + user_hobby.title() + '\n')
+            f_obj.write(str(id + 1) + ' Name: ' + user_name.title() + ' Hobby: ' + user_hobby.title() + '\n')
+except:
+    with open(f_users, 'w') as f_obj:
+        f_obj.write(str(1) + ' Name: ' + user_name.title() + ' Hobby: ' + user_hobby.title() + '\n')
+
